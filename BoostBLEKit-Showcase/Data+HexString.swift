c5690b62c74ca9f80f8fcb8c58bdf896da5bd5c4
Shinichiro Oba <ooba@bricklife.com>
2018-07-05 00:26:47 +0100

    Enable control Boost

---
+++ BoostBLEKit-Showcase/Data+HexString.swift
@@ -0,0 +1,30 @@
+//
+//  Data+HexString.swift
+//  BoostRemote
+//
+//  Created by ooba on 10/08/2017.
+//  Copyright © 2017 bricklife.com. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    
+    public init?(hexString: String) {
+        let hexString = hexString
+            .replacingOccurrences(of: "0x", with: "")
+            .components(separatedBy: CharacterSet(charactersIn: "0123456789abcdefABCDEF").inverted)
+            .joined()
+        
+        let even = hexString.enumerated().filter { $0.offset % 2 == 0 }.map { $0.element }
+        let odd  = hexString.enumerated().filter { $0.offset % 2 == 1 }.map { $0.element }
+        
+        let bytes = zip(even, odd).compactMap { UInt8(String([$0.0, $0.1]), radix: 16) }
+        
+        self.init(bytes: bytes)
+    }
+    
+    public var hexString: String {
+        return map { String(format: "%02x", $0) }.joined(separator: " ")
+    }
+}
--- BoostBLEKit-Showcase/ViewController.swift
+++ BoostBLEKit-Showcase/ViewController.swift
@@ -7,21 +7,100 @@
 //
 
 import Cocoa
+import BoostBLEKit
+import CoreBluetooth
+
+struct MoveHubService {
+    
+    static let serviceUuid = CBUUID(string: GATT.serviceUuid)
+    static let characteristicUuid = CBUUID(string: GATT.characteristicUuid)
+}
 
 class ViewController: NSViewController {
     
     @IBOutlet weak var label: NSTextField!
     
-    var power: Int = 0
+    private var centralManager: CBCentralManager!
+    private var peripheral: CBPeripheral?
+    private var characteristic: CBCharacteristic?
+    
+    private var motors: [BoostBLEKit.Port : Motor] = [:]
+    private var power: Int8 = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
         setPower(power: 0)
     }
     
-    func setPower(power: Int) {
+    private func setPower(power: Int8) {
         self.power = power
         label.stringValue = "\(power)"
+        
+        if let command = motors[.A]?.powerCommand(power: power) {
+            print("<", command.data.hexString)
+            write(data: command.data)
+        }
+    }
+    
+    func startScan() {
+        if centralManager.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: [MoveHubService.serviceUuid], options: nil)
+        }
+    }
+    
+    func stopScan() {
+        centralManager.stopScan()
+    }
+    
+    func connect(peripheral: CBPeripheral) {
+        if self.peripheral == nil {
+            self.peripheral = peripheral
+            centralManager.connect(peripheral, options: nil)
+        }
+    }
+    
+    func disconnect() {
+        if let peripheral = peripheral {
+            centralManager.cancelPeripheralConnection(peripheral)
+            self.peripheral = nil
+            self.characteristic = nil
+        }
+    }
+    
+    func set(characteristic: CBCharacteristic) {
+        if let peripheral = peripheral, characteristic.properties.contains([.write, .notify]) {
+            self.characteristic = characteristic
+            peripheral.setNotifyValue(true, for: characteristic)
+        }
+    }
+    
+    func receive(notification: BoostBLEKit.Notification) {
+        switch notification {
+        case .connected(let port, let deviceType):
+            print("connected:", port, deviceType)
+            if let motor = Motor(port: port, deviceType: deviceType) {
+                motors[port] = motor
+            }
+        case .disconnected(let port):
+            print("disconnected:", port)
+        }
+    }
+    
+    func write(data: Data) {
+        if let peripheral = peripheral, let characteristic = characteristic {
+            peripheral.writeValue(data, for: characteristic, type: .withResponse)
+        }
+    }
+    
+    @IBAction func pushConnectButton(_ sender: Any) {
+        if peripheral == nil {
+            startScan()
+        } else {
+            disconnect()
+        }
     }
     
     @IBAction func pushPlusButton(_ sender: Any) {
@@ -36,5 +115,66 @@ class ViewController: NSViewController {
     
     @IBAction func pushStopButton(_ sender: Any) {
         setPower(power: 0)
+    }
+}
+
+extension ViewController: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("unknown")
+        case .resetting:
+            print("resetting")
+        case .unsupported:
+            print("unsupported")
+        case .unauthorized:
+            print("unauthorized")
+        case .poweredOff:
+            print("poweredOff")
+        case .poweredOn:
+            print("poweredOn")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        connect(peripheral: peripheral)
+        stopScan()
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices([MoveHubService.serviceUuid])
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print(#function, peripheral.identifier, error ?? "nil")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print(#function, peripheral.identifier, error ?? "nil")
+    }
+}
+
+extension ViewController: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let service = peripheral.services?.first(where: { $0.uuid == MoveHubService.serviceUuid }) {
+            peripheral.discoverCharacteristics([MoveHubService.characteristicUuid], for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristic = service.characteristics?.first(where: { $0.uuid == MoveHubService.characteristicUuid }) {
+            set(characteristic: characteristic)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        guard let data = characteristic.value else { return }
+        print(">", data.hexString)
+        if let notification = Notification(data: data) {
+            receive(notification: notification)
+        }
     }
 }
